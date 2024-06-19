9fd508ccdb9096bf194fd0e606609ad8fab17adb
namomi <xicodey@gmail.com>
2024-06-19 10:38:25 +0900

    프로젝트 생성

---
+++ src/main/java/com/web_springservice/web/WebSpringserviceApplication.java
@@ -0,0 +1,13 @@
+package com.web_springservice.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSpringserviceApplication.class, args);
+	}
+
+}
